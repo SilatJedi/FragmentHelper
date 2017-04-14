44ffc3f75e11b6ccdd1ce8a619c464dcbb2bce95
James Anderson <SilatJedi@users.noreply.github.com>
2017-04-13 18:02:36 -0600

    init

---
+++ FragmentHelper.java
@@ -0,0 +1,23 @@
+package com.silatsaktistudios.Fragments
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by SilatJedi on 4/13/17.
+ */
+
+public class FragmentHelper {
+
+
+	/*
+	/Example: FragmentHelper.setFragment(MainActivity.this, DemoFragment.newInstance(), R.id.fragment_container); 
+	*/
+    public static void setFragment(FragmentActivity activity, Fragment newFragment, int layoutId) {
+        FragmentTransaction ft = activity.getSupportFragmentManager().beginTransaction();
+          ft.replace(layoutId, newFragment)
+            .addToBackStack(null)
+            .commit();
+    }
+}
